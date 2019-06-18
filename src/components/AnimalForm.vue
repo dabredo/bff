91c1b299afd3e705ffbc6aa19b2f438119562473
David <da.breva@gmail.com>
2019-06-18 23:13:38 +0200

    Create component AnimalForm

---
+++ src/components/AnimalForm.vue
@@ -0,0 +1,120 @@
+<template>
+  <v-form v-if="selectedFriend" class="mb-2" v-model="valid">
+    <v-text-field
+      v-model="selectedFriend.name"
+      :counter="100"
+      v-validate="'required|max:100'"
+      :error-messages="errors.collect('name')"
+      data-vv-name="name"
+      data-vv-as="Nombre"
+      data-vv-validate-on="change"
+      label="Nombre"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="selectedFriend.breed"
+      label="Raza"
+      required
+    ></v-text-field>
+
+    <v-radio-group v-model="selectedFriend.gender" label="Sexo">
+      <v-radio label="Macho" value="m"></v-radio>
+      <v-radio label="Hembra" value="f"></v-radio>
+    </v-radio-group>
+
+    <v-select v-model="selectedFriend.size"
+      :items="sizes"
+      label="Tamano"
+    ></v-select>
+
+    <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      :nudge-right="40"
+      lazy
+      transition="scale-transition"
+      offset-y
+      full-width
+      min-width="290px"
+    >
+      <template v-slot:activator="{ on }">
+        <v-text-field
+          :value="dateFormatted"
+          label="Fecha de nacimiento"
+          prepend-icon="event"
+          clearable
+          readonly
+          v-on="on"
+          ></v-text-field>
+      </template>
+      <v-date-picker
+        v-model="selectedFriend.birthdate"
+        no-title
+        @input="menu = false"
+      ></v-date-picker>
+    </v-menu>
+
+    <v-select v-model="selectedFriend.state"
+      :items="states"
+      label="Estado"
+    ></v-select>
+
+    <v-textarea
+      v-model="selectedFriend.description"
+      :counter="1000"
+      v-validate="'max:1000'"
+      :error-messages="errors.collect('description')"
+      data-vv-name="description"
+      data-vv-as="Descripcion"
+      label="Descripcion"
+      data-vv-validate-on="change"
+    ></v-textarea>
+
+    <v-btn color="primary" :disabled="!valid" v-on:click="saveFriend(selectedFriend)" class="primar">Guardar</v-btn>
+    <v-btn v-on:click="cancel()">Cancelar</v-btn>
+  </v-form>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'AnimalForm',
+  data: function() {
+    return {
+      valid: true,
+      menu: false,
+      states: [
+        { value: 'not_adopted', text: 'No adoptado' },
+        { value: 'adopted', text: 'Adoptado' }
+      ],
+    }
+  },
+  computed: {
+    ...mapState('animalShelter', [ 'selectedFriend', 'sizes' ]),
+    dateFormatted() {
+      return this.selectedFriend.birthdate ? this.$moment(this.selectedFriend.birthdate).format('DD/MM/YYYY') : ''
+    },
+  },
+  methods: {
+    async saveFriend(friend) {
+      let valid = await this.$validator.validate();
+      if (!valid) {
+        return;
+      }
+
+      if (friend.id) {
+        await this.$store.dispatch('animalShelter/updateFriend', friend)
+      } else {
+        await this.$store.dispatch('animalShelter/createFriend', friend)
+      }
+
+      this.$store.commit('notification/displaySuccess', "Friend was saved")
+    },
+
+    async cancel() {
+      this.$store.commit('animalShelter/unselectFriend')
+    },
+  }
+}
+</script>
--- src/components/AnimalShelter.vue
+++ src/components/AnimalShelter.vue
@@ -1,128 +1,63 @@
 <template>
-    <v-container>
-        <h2 class="headline font-weight-bold mb-2">
-            Amigos
+  <v-container>
+    <h2 class="headline font-weight-bold mb-2">
+      Amigos
 
-            <v-btn v-if="!selectedFriend" color="primary" dark v-on:click="displayAddFriend">
-                Anadir
-            </v-btn>
-        </h2>
+      <v-btn v-if="!selectedFriend" color="primary" dark v-on:click="displayAddFriend">
+        Anadir
+      </v-btn>
+    </h2>
 
-        <v-form v-if="selectedFriend" class="mb-2" v-model="valid">
-            <v-text-field
-                v-model="selectedFriend.name"
-                :counter="100"
-                v-validate="'required|max:100'"
-                :error-messages="errors.collect('name')"
-                data-vv-name="name"
-                data-vv-as="Nombre"
-                data-vv-validate-on="change"
-                label="Nombre"
-            ></v-text-field>
+    <AnimalForm />
 
-            <v-text-field
-                v-model="selectedFriend.breed"
-                label="Raza"
-                required
-            ></v-text-field>
+    <v-data-table v-if="friendsCount" :headers="headers" :items="friends">
+      <template v-slot:items="props">
+        <tr>
+          <td>{{ props.item.name }}</td>
+          <td>{{ props.item.breed }}</td>
+          <td>
+            <template v-if="props.item.gender === 'm'">Macho</template><template v-else>Hembra</template>
+          </td>
+          <td>
+            <template v-if="props.item.size === 'small'">Pequeno</template>
+            <template v-else-if="props.item.size === 'medium'">Mediano</template>
+            <template v-else>Grande</template>
+          </td>
+          <td v-if="props.item.birthdate">{{ props.item.birthdate | moment('DD/MM/YYYY') }}</td>
+          <td v-else>-</td>
+          <td>
+            <template v-if="props.item.state === 'not_adopted'">No adoptado</template>
+            <template v-else-if="props.item.state === 'adopted'">Adoptado</template>
+          </td>
+          <td>{{ props.item.createdAt | moment('DD/MM/YYYY') }} </td>
+          <td class="text-xs-right">
+            <v-icon small class="mr-2" v-on:click="viewFriend(props.item.id)">
+              edit
+            </v-icon>
+            <v-icon small class="mr-2" v-on:click="deleteFriend(props.item.id)">
+              delete
+            </v-icon>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
 
-            <v-radio-group v-model="selectedFriend.gender" label="Sexo">
-                <v-radio label="Macho" value="m"></v-radio>
-                <v-radio label="Hembra" value="f"></v-radio>
-            </v-radio-group>
-
-            <v-select v-model="selectedFriend.size"
-                :items="sizes"
-                label="Tamano"
-            ></v-select>
-
-            <v-menu
-              v-model="menu"
-              :close-on-content-click="false"
-              :nudge-right="40"
-              lazy
-              transition="scale-transition"
-              offset-y
-              full-width
-              min-width="290px"
-            >
-              <template v-slot:activator="{ on }">
-                <v-text-field
-                  :value="dateFormatted"
-                  label="Fecha de nacimiento"
-                  prepend-icon="event"
-                  clearable
-                  readonly
-                  v-on="on"
-                  ></v-text-field>
-              </template>
-              <v-date-picker v-model="selectedFriend.birthdate" no-title @input="menu = false"></v-date-picker>
-            </v-menu>
-
-            <v-select v-model="selectedFriend.state"
-                :items="states"
-                label="Estado"
-            ></v-select>
-
-            <v-textarea
-                v-model="selectedFriend.description"
-                :counter="1000"
-                v-validate="'max:1000'"
-                :error-messages="errors.collect('description')"
-                data-vv-name="description"
-                data-vv-as="Descripcion"
-                label="Descripcion"
-                data-vv-validate-on="change"
-            ></v-textarea>
-
-            <v-btn color="primary" :disabled="!valid" v-on:click="saveFriend(selectedFriend)" class="primar">Guardar</v-btn>
-            <v-btn v-on:click="cancel()">Cancelar</v-btn>
-        </v-form>
-
-        <v-data-table v-if="friendsCount" :headers="headers" :items="friends">
-            <template v-slot:items="props">
-                <tr>
-                    <td>{{ props.item.name }}</td>
-                    <td>{{ props.item.breed }}</td>
-                    <td>
-                        <template v-if="props.item.gender === 'm'">Macho</template><template v-else>Hembra</template>
-                    </td>
-                    <td>
-                        <template v-if="props.item.size === 'small'">Pequeno</template>
-                        <template v-else-if="props.item.size === 'medium'">Mediano</template>
-                        <template v-else>Grande</template>
-                    </td>
-                    <td v-if="props.item.birthdate">{{ props.item.birthdate | moment('DD/MM/YYYY') }}</td>
-                    <td v-else>-</td>
-                    <td>
-                        <template v-if="props.item.state === 'not_adopted'">No adoptado</template>
-                        <template v-else-if="props.item.state === 'adopted'">Adoptado</template>
-                    </td>
-                    <td>{{ props.item.createdAt | moment('DD/MM/YYYY') }} </td>
-                    <td class="text-xs-right">
-                        <v-icon small class="mr-2" v-on:click="viewFriend(props.item.id)">
-                            edit
-                        </v-icon>
-                        <v-icon small class="mr-2" v-on:click="deleteFriend(props.item.id)">
-                            delete
-                        </v-icon>
-                    </td>
-                </tr>
-            </template>
-        </v-data-table>
-
-        <p v-else>There are not friends</p>
-    </v-container>
+    <p v-else>There are not friends</p>
+  </v-container>
 </template>
 
 <script>
 import { mapState, mapGetters } from 'vuex';
 
+import AnimalForm from '../components/AnimalForm'
+
 export default {
+  components: {
+    AnimalForm
+  },
   name: 'AnimalShelter',
   data: function() {
     return {
-      menu: false,
       headers: [
         { text: 'Nombre', value: 'name' },
         { text: 'Raza', value: 'breed'  },
@@ -137,15 +72,11 @@ export default {
         { value: 'not_adopted', text: 'No adoptado' },
         { value: 'adopted', text: 'Adoptado' }
       ],
-      valid: true,
     }
   },
   computed: {
-    ...mapState('animalShelter', [ 'friends', 'selectedFriend', 'sizes' ]),
+    ...mapState('animalShelter', [ 'friends', 'selectedFriend' ]),
     ...mapGetters('animalShelter', [ 'friendsCount' ]),
-    dateFormatted() {
-      return this.selectedFriend.birthdate ? this.$moment(this.selectedFriend.birthdate).format('DD/MM/YYYY') : ''
-    },
   },
   created () {
       this.$store.dispatch('animalShelter/getFriends');
@@ -155,35 +86,16 @@ export default {
       this.$store.commit('animalShelter/selectNewFriend')
     },
 
-    async saveFriend(friend) {
-      let valid = await this.$validator.validate();
-      if (!valid) {
-        return;
-      }
-
-      if (friend.id) {
-        await this.$store.dispatch('animalShelter/updateFriend', friend)
-      } else {
-        await this.$store.dispatch('animalShelter/createFriend', friend)
-      }
-
-      this.$store.commit('notification/displaySuccess', "Friend was saved")
-    },
-
-    async cancel() {
-      this.$store.commit('animalShelter/unselectFriend')
-    },
-
     async viewFriend(friendId) {
       this.$store.dispatch('animalShelter/getFriend', friendId)
     },
 
     async deleteFriend(friendId) {
       if (confirm('Are you sure?')) {
-        this.cancel()
+        this.$store.commit('animalShelter/unselectFriend')
 
-         await this.$store.dispatch('animalShelter/deleteFriend', friendId)
-         this.$store.commit('notification/displaySuccess', "Friend was removed")
+        await this.$store.dispatch('animalShelter/deleteFriend', friendId)
+        this.$store.commit('notification/displaySuccess', "Friend was removed")
       }
     }
   }
